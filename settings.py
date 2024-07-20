0ed474a99813ff0bf3bc658f111bda00b86ba8bf
Kacper Beirowski <kacper.beirowski@gmail.com>
2024-07-20 13:55:22 +0200

    ADD: Settings module

---
+++ settings.py
@@ -0,0 +1,7 @@
+class Settings:
+    """Stored all game settings"""
+    def __init__(self):
+        """Init game settings"""
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
