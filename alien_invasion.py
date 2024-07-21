04e547712b56dbad46c553d8d990123d3211ce25
Kacper Beirowski <kacper.beirowski@gmail.com>
2024-07-21 19:45:28 +0200

    ADD: Shooting bullets from player's ship

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     """Main config game class"""
@@ -19,6 +20,7 @@ class AlienInvasion:
         # self.settings.screen_width = self.screen.get_rect().width
         # self.settings.screen_height = self.screen.get_rect().height
         self.player_ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
         pygame.display.set_caption("Alien Invasion")
 
     def _check_keydown_events(self, event):
@@ -27,6 +29,8 @@ class AlienInvasion:
             self.player_ship.moving_right = True
         elif event.key == pygame.K_LEFT:
             self.player_ship.moving_left = True
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
         elif event.key == pygame.K_q:
             sys.exit()
 
@@ -48,19 +52,36 @@ class AlienInvasion:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
+    def _update_bullets(self):
+        """Updating bullets position on screen, deleting invisible bullets"""
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+            if bullet.rect.bottom < 0:
+                self.bullets.remove(bullet)
+
     def _update_screen(self):
-        """Updateing screen"""
+        """Updating screen"""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         self.player_ship.blitme()
         pygame.display.flip()
+
+    def _fire_bullet(self):
+        """Create new bullet and append to the group"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
 
     def run_game(self):
         """Main game loop"""
         while True:
             self._check_events()
             self.player_ship.update()
+            self._update_bullets()
             self._update_screen()
             self.clock.tick(60)
+
 
 if __name__ == "__main__":
     ai = AlienInvasion()
